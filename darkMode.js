a223d1585e7e97eb73f3486db83879fcc6ac746d
epvillar <villar_esteban_pedro@hotmail.com>
2022-04-24 16:38:47 -0300

    'first'

---
+++ darkMode.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.querySelector('.btn');
+/*const articlesContainer = document.querySelector('.articles');*/
+
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-theme');
+});
+
---
+++ readme.txt
@@ -0,0 +1,2 @@
+This project is a clone of TechDocumentation plus J.Smilga Vanilla
+ JS proyect Dark Mode
